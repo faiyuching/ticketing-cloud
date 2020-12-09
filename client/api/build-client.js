4fe00941be5df13a0fb3881a29228c38159f39ec
faiyuching <faiyuching@magic.local>
2020-12-09 21:42:21 +0800

    update

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
-
     return axios.create({
       baseURL:
         'https://www.zhuominginfo.org/',
